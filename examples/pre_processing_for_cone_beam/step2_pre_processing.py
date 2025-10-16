64c57d5c7ec3d0e3472bcfa279fca828db11eb9c
nghia-vo <nvo@bnl.gov>
2025-10-16 12:04:25 -0400

    Update examples

--- examples/pre_processing_for_cone_beam/step2_pre_processing.py
+++ examples/pre_processing_for_cone_beam/step2_pre_processing.py
@@ -5,6 +5,7 @@ import numpy as np
 import algotom.io.loadersaver as losa
 import algotom.prep.removal as rem
 import algotom.prep.correction as corr
+import algotom.util.utility as util
 
 input_file = "E:/tmp/projections.hdf"
 output_file = "E:/tmp/tmp/projections_preprocessed.hdf"
@@ -23,28 +24,36 @@ chunk_size = np.clip(ncore - 1, 1, height - 1)
 last_chunk = height - chunk_size * (height // chunk_size)
 for i in np.arange(0, height - last_chunk, chunk_size):
     sinograms = np.float32(data[:, i:i + chunk_size, :])
-    # Note about the change of the shape of output_tmp (which is a list of processed sinogram)
-    output_tmp = Parallel(n_jobs=ncore, prefer="threads")(delayed(rem.remove_all_stripe)(sinograms[:, j, :], 3.0, 51, 21) for j in range(chunk_size))
-
+    sinograms = util.parallel_process_slices(sinograms,
+                                             rem.remove_all_stripe,
+                                             [3.0, 51, 21],
+                                             ncore=ncore, prefer="processes",
+                                             axis=1)
     # Apply beam hardening correction if need to
-    # output_tmp = np.asarray(output_tmp)
-    # output_tmp = Parallel(n_jobs=ncore, prefer="threads")(
-    #     delayed(corr.beam_hardening_correction)(output_tmp[j], 40, 2.0, False) for j in range(chunk_size))
-
-    output[i:i + chunk_size] = np.asarray(output_tmp, dtype=np.float32)
+    # sinograms = util.parallel_process_slices(sinograms,
+    #                                          corr.beam_hardening_correction,
+    #                                          [40, 2.0, False],
+    #                                          ncore=ncore, prefer="processes",
+    #                                          axis=1)
+    output[i:i + chunk_size] = np.moveaxis(sinograms, 1, 0)
     t1 = timeit.default_timer()
     print("Done sinograms: {0}-{1}. Time {2}".format(i, i + chunk_size, t1 - t0))
 
 if last_chunk != 0:
     sinograms = np.float32(data[:, height - last_chunk:height, :])
-    output_tmp = Parallel(n_jobs=ncore, prefer="threads")(delayed(rem.remove_all_stripe)(sinograms[:, j, :], 3.0, 51, 21) for j in range(last_chunk))
+    sinograms = util.parallel_process_slices(sinograms,
+                                             rem.remove_all_stripe,
+                                             [3.0, 51, 21],
+                                             ncore=ncore, prefer="processes",
+                                             axis=1)
 
     # Apply beam hardening correction if need to
-    # output_tmp = np.asarray(output_tmp)
-    # output_tmp = Parallel(n_jobs=ncore, prefer="threads")(
-    #     delayed(corr.beam_hardening_correction)(output_tmp[j], 40, 2.0, False) for j in range(last_chunk))
-
-    output[height - last_chunk:height] = np.asarray(output_tmp, dtype=np.float32)
+    # sinograms = util.parallel_process_slices(sinograms,
+    #                                          corr.beam_hardening_correction,
+    #                                          [40, 2.0, False],
+    #                                          ncore=ncore, prefer="processes",
+    #                                          axis=1)
+    output[height - last_chunk:height] = np.moveaxis(sinograms, 1, 0)
     t1 = timeit.default_timer()
     print("Done sinograms: {0}-{1}. Time {2}".format(height - last_chunk, height - 1, t1 - t0))
 
